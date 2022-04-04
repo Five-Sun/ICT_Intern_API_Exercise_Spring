e45e6ee2e1f59f0c7fd1852c9e1dffc4f49a7989
Oh-Tae-Yang <87643153+Five-Sun@users.noreply.github.com>
2022-04-04 21:35:41 +0900

    Update search.js

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,10 +1,10 @@
 function getYoutube(title, artist) {
-    var API_KEY = "AIzaSyDQSVysWES54RDYnjZtfbmftS1K6WlBpVQ";
+    var API_KEY = "";
     var search = title + artist;
     var maxResults = 1
 
 
-    API_KEY = "API_개인키";
+    API_KEY = "AIzaSyDQSVysWES54RDYnjZtfbmftS1K6WlBpVQ";
 
     var url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}
           &part=snippet&q=${search}&maxResults=${maxResults}&type=video`;
